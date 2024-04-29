5646545f48c5c09611bceb38e7bcd7eaa7789090
Mauricio Gonzalez <maugonzal@gmail.com>
2024-04-28 18:18:13 -0600

    Add files via upload

--- model_deployment/api.py
+++ model_deployment/api.py
@@ -10,21 +10,21 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes and origins
 
 api = Api(
-    app, 
-    version='1.0', 
-    title='Phishing Prediction API',
-    description='Phishing Prediction API')
+    app,
+    version='1.0',
+    title='API Predicción de Precio',
+    description='API para la predicción del vehículo usado')
 
-ns = api.namespace('predict', 
-     description='Phishing Classifier')
-   
+ns = api.namespace('predict',
+                   description='Estimación Precio')
+
 parser = api.parser()
 
 parser.add_argument(
-    'URL', 
-    type=str, 
-    required=True, 
-    help='URL to be analyzed', 
+    'X',
+    type=str,
+    required=True,
+    help='Variables del vehículo',
     location='args')
 
 resource_fields = api.model('Resource', {
@@ -38,11 +38,11 @@ class PhishingApi(Resource):
     @api.marshal_with(resource_fields)
     def get(self):
         args = parser.parse_args()
-        
+
         return {
-         "result": predict_proba(args['URL'])
+            "result": predict_proba(args['X'])
         }, 200
-    
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
